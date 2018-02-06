fea479e3f6239e026ca73c78aa347440ad42e567
Renan Lage <renanlage@gmail.com>
2018-02-06 11:45:35 -0200

    Reflect exercises made in filenames

--- exercises/functions_2,3.exs
+++ exercises/functions_2,3.exs
@@ -5,6 +5,11 @@ fizzbuzz = fn
   (_, _, c) -> c
 end
 
+IO.puts fizzbuzz.(0, 0, 5) == "FizzBuzz"
+IO.puts fizzbuzz.(0, 5, 5) == "Fizz"
+IO.puts fizzbuzz.(5, 0, 5) == "Buzz"
+IO.puts fizzbuzz.(5, 5, 10) == 10
+
 fizzbuzz_solver = fn(n) ->
   fizzbuzz.(rem(n, 3), rem(n, 5), n)
 end
