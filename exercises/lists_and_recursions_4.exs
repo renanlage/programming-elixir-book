9aa6bc7e512e3d9858b2e8c2122a6eae538ed1a3
Renan Lage <renanlage@gmail.com>
2018-02-16 13:21:26 -0200

    Add option to build reversed list in exercise 4

--- exercises/lists_and_recursions_4.exs
+++ exercises/lists_and_recursions_4.exs
@@ -6,8 +6,15 @@ end
 defmodule MyListTailCall do
   def span(from, to), do: _span(from, to, [])
 
-  def _span(from, to, list) when from > to, do: list
-  def _span(from, to, list), do: _span(from, to - 1, [to | list])
+  defp _span(from, to, list) when from > to, do: list
+  defp _span(from, to, list), do: _span(from, to - 1, [to | list])
+end
+
+defmodule MyListAllowReversed do
+  def span(from, from), do: [from]
+
+  def span(from, to) when to > from, do: [from | span(from + 1, to)]
+  def span(from, to) when to < from, do: [from | span(from - 1, to)]
 end
 
 IO.puts(MyList.span(1, 1) == [1])
@@ -18,3 +25,7 @@ IO.puts(MyListTailCall.span(1, 1) == [1])
 IO.puts(MyListTailCall.span(1, 2) == [1, 2])
 IO.puts(MyListTailCall.span(2, 5) == [2, 3, 4, 5])
 IO.puts(MyListTailCall.span(5, 2) == [])
+IO.puts(MyListAllowReversed.span(1, 1) == [1])
+IO.puts(MyListAllowReversed.span(1, 2) == [1, 2])
+IO.puts(MyListAllowReversed.span(2, 5) == [2, 3, 4, 5])
+IO.puts(MyListAllowReversed.span(5, 2) == [5, 4, 3, 2])
