dc265282ef3beb2aaf0f81a98bdbd3df2a78fb66
Renan Lage <renanlage@gmail.com>
2018-03-13 20:49:28 -0300

    Add issues CLI command line parser

---
+++ projects/issues/lib/issues/cli.ex
@@ -0,0 +1,42 @@
+defmodule Issues.CLI do
+  @default_count 4
+
+  @moduledoc """
+  Handle the command line parsing and the dispatch to
+  the various functions that end up generating a
+  table of the last_n_issues in a github project
+  """
+
+  def run(argv) do
+    argv
+    |> parse_args
+    |> process
+  end
+
+  def process(:help) do
+    IO.puts "usage: issues <user> <project> [ count | #{@default_count} ]"
+    System.halt(0)
+  end
+
+  def process({ user, project, count }) do
+    Issues.GithubIssues.fetch(user, project)
+  end
+
+  @doc """
+  `argv` can be -h or --help, which returns :help.
+
+  Otherwise it is a github username, project name, and (optionally)
+  the number of entries to format.
+
+  Return a tuple of `{ user, project, count }`, or `:help` if help was given
+  """
+  def parse_args(argv) do
+    parse = OptionParser.parse(argv, switches: [help: :boolean], aliases: [h: :help])
+    case parse do
+      { [help: true], _, _ } -> :help
+      { _, [user, project, count], _ } -> { user, project, String.to_integer(count) }
+      { _, [user, project], _ } -> { user, project, @default_count }
+      _ -> :help
+    end
+  end
+end
