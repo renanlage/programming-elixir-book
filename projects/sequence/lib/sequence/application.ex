4da8d5cbdcebe077c1156d494c270d88b7b9cdc9
Renan Lage <renanlage@gmail.com>
2018-04-17 14:57:20 -0300

    Preserve state on crash in sequence server

--- projects/sequence/lib/sequence/application.ex
+++ projects/sequence/lib/sequence/application.ex
@@ -6,13 +6,6 @@ defmodule Sequence.Application do
   use Application
 
   def start(_type, _args) do
-    import Supervisor.Spec, warn: false
-
-    children = [
-      worker(Sequence.Server, [123])
-    ]
-
-    opts = [strategy: :one_for_one, name: Sequence.Supervisor]
-    {:ok, _pid} = Supervisor.start_link(children, opts)
+    {:ok, _pid} = Sequence.Supervisor.start_link(123)
   end
 end
--- projects/sequence/lib/sequence/server.ex
+++ projects/sequence/lib/sequence/server.ex
@@ -3,8 +3,8 @@ defmodule Sequence.Server do
 
   # External API
 
-  def start_link(current_number) do
-    GenServer.start_link(__MODULE__, current_number, name: __MODULE__)
+  def start_link(stash_pid) do
+    GenServer.start_link(__MODULE__, stash_pid, name: __MODULE__)
   end
 
   def next_number do
@@ -17,12 +17,21 @@ defmodule Sequence.Server do
 
   # GenServer implementation
 
-  def handle_call(:next_number, _from, current_number) do
-    {:reply, current_number, current_number + 1}
+  def init(stash_pid) do
+    current_number = Sequence.Stash.get_value stash_pid
+    {:ok, {current_number, stash_pid}}
   end
 
-  def handle_cast({:increment_number, delta}, current_number) do
-    {:noreply, current_number + delta}
+  def handle_call(:next_number, _from, {current_number, stash_pid}) do
+    {:reply, current_number, {current_number + 1, stash_pid}}
+  end
+
+  def handle_cast({:increment_number, delta}, {current_number, stash_pid}) do
+    {:noreply, {current_number + delta, stash_pid}}
+  end
+
+  def terminate(_reason, {current_number, stash_pid}) do
+    Sequence.Stash.save_value stash_pid, current_number
   end
 
   def format_status(_reason, [_pdit, state]) do
